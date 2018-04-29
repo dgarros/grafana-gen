740c1b5874f5049891ae9c1e5a43ac541cdd1732
Damien Garros <dgarros@gmail.com>
2018-04-29 13:00:08 -0700

    Update requirement file and fix issue with pip10

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ import sys
 import uuid
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
-
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+    
 __author__ = 'Damien Garros <dgarros@gmail.com>'
 
 requirements_data = parse_requirements('requirements.txt', session=uuid.uuid1())
